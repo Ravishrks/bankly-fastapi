5866ffb0314c56315ea4bde5cf55aa7992a8bc34
Ravish <oceancusp@gmail.com>
2023-02-01 22:22:42 +0530

    sync

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def send_test_request():
     # Decrypting encrypted key to get session key,
     # to be used in AES decryption
     random_string = 'fgetdh3564hegde5'
-    session_key_in_bytes = b64encode(random_string)
+    session_key_in_bytes = b64decode(random_string)
     # session_key_in_bytes = get_random_bytes(16)
     encrypted_session_key_in_bytes = encrypt_using_public_key(
         session_key_in_bytes)
