24c9073e20016204ce2ad6af40b6c3693c987185
Ravish <oceancusp@gmail.com>
2023-02-02 11:24:40 +0530

    sync

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from Crypto.Util.Padding import pad
 import http3
 import rsa
 import requests
+import json
 
 
 # client = http3.AsyncClient()
@@ -77,7 +78,7 @@ async def send_test_request():
                'apikey': 'xUHvlTOtkLn37jnuG0Yp8zr2kivgRg6j', 'SrcApp': 'bankly', 'Accept': 'application/json'}
 
     # r = await client.post(endpoint_url, data=request_data, headers=headers)
-    r = requests.post('https://apibankingonesandbox.icicibank.com/api/v1/pcms-chw?service=LinkedMobile', data='{"requestId": "","service": "","encryptedKey": "jXwyhjY7tMmQuxFQCBq6lHTUbW4OjD01G88mAZEtfqXi2L8NDnFWIO44bcx2b7OEO14YPlBtn8MqpgsZNNCoZ+/jru8BuPOfF0aSFx060qNA8dDXaQ6sVtyV2xiT3/WrKy3m5kQAVmygy7PqIeV/rSOA4rH0SNNZuz996DEj3yub9YW327Af1Rh5nysADKJSiBdZpbCFAYaALEnU+sG/Y67bp60lEd27ek84f1cdb+EzwWcv78ISBSefmZ5QfSsLUAYzNE6BEX2Bps7pIXjdrD3L+4+Rl8WNCCUPZFtPYKK5KnvO/wICfN7Qo3/h1IHr6Ni77KUilFogV2YZMeL0xAppvgXlK7A0Cz8rnLv1JE/Ha7nvHxCf8kGQKuSBUnGiufLj0yLFR+3us58NzeX6s7iMByHfRMRcRDzmWcGel85cfwhUSzaBfJVsguohmNKqNgpphaR5s4FoHtRTzHEP83dAFR6edmtQ70d3Au6areI5XPi7aJJ7gttDnuciRXmzmpB4heWmlqd1wxAg+bsGyHRMGt2bGNBFQUYyGOJPkWCM8Zl+LTASFqpoyrlly5JnPIN5IMjzf9WNXm8L3wjQMt5KRRmFl5NGJUsDMGOWQRfiszDxBUtpmimfuo/CVddOu0htChvh5CU1L/8EFkWDZZrt2VWR/taA9ja4X0/wOUw=","oaepHashingAlgorithm": "NONE","iv": "","encryptedData": "OTY3OTI4N2MyMjc0ODQ1ONHs9UlFeXlM6wOKEsTYfn0DAsm9Z7KIl2fqgoZyXgLDQVTQ00fIUmTBw1T27DxGBnSF2Z/hq6wF+d+TeH6XOKLl9uCuvo8C9/aHBP0eDT5xf8ohFYSaZzfmPVr+sGZ/tjuhrnu8mSqbK90RCbuYRj2EjBFbhnB21VmXgYYHAQIt0Oqhp/F3yCyLlRvv7PvfHSy5LruVCjrbLK2W1IxdGDa+b+cq1A5u3tfz96orVtZpQnz55ZTh8MXoT4q8bHPA4W6g7924oCzxIVxRH/yY2x7RWrxwLwWDc3/QgQfT21xo7yq00j2soR95OjhIIsDz2ZzY2JyymOo+ckldMxX0JDHP56opVI+GTvO2fgIxG8p+tMD3A2yyq350/os+lJtmc8LzihhkzVfrFCFvUvMoK5qw/8uFb9Q7auZTdT65PKMF","clientInfo": "","optionalParam": ""}', headers=headers)
+    r = requests.post('https://apibankingonesandbox.icicibank.com/api/v1/pcms-chw?service=LinkedMobile', data=json.dumps(request_data), headers=headers)
 
     print(r.status_code)
     print(r.text)
